eeb12514dd72370b24bdfc9b0323dd900257b52a
garpRojas <grojaspineda@gmail.com>
2017-10-24 07:16:49 -0600

    Updating git becuase was never updated

--- js/angular/controller.js
+++
@@ -1,9 +0,0 @@
-var leonesProfile = angular.module("LeonesProfile" , []);
-
-leonesProfile.controller('CarouselControler', function MyController($scope, $http){
-
-	$http.get('js/angular/players.json').then(function(response){
-		$scope.player = response.data;
-	});
-	
-});
